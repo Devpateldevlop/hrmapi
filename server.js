6b8aa9f9c331d173f9b0965e9920305a68a7f31b
Devpateldevlop <devpatel190703@gmail.com>
2025-02-10 16:43:16 +0530

    co

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ app.use(cors());
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');  // Frontend URL
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT')
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();  // Handle preflight request
