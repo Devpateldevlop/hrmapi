c65926a56449833cbdfb58ea34b4946649419e8d
Devpateldevlop <devpatel190703@gmail.com>
2025-02-11 12:11:58 +0530

    fwff

--- api/punchHistory.js
+++ api/punchHistory.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const PunchHistory = require('../model/PunchHistory'); // Assuming model is in models folder
+const cors = require('cors');
+const app = express();
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({orgin:'*'}));
 module.exports = async (req, res) => {
   if (req.method === 'GET') {
     try {
