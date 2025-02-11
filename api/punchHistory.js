bd9e720fad51684186ccf52b62aa754038080267
Devpateldevlop <devpatel190703@gmail.com>
2025-02-11 12:02:55 +0530

    frgrw

--- api/punchHistory.js
+++ api/punchHistory.js
@@ -1,29 +1,20 @@
-const mongoose = require("mongoose");
-const PunchHistory = require('../model/PunchHistory');
-const connectToMongoDB = async () => {
-  const uri = process.env.MONGO_URI;
-  if (mongoose.connections[0].readyState) {
-    console.log("Already connected to MongoDB");
-    return;
-  }
+const mongoose = require('mongoose');
+const PunchHistory = require('../model/PunchHistory'); // Assuming model is in models folder
 
-  try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("MongoDB connected!");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      const punchHistories = await PunchHistory.find();
+      res.status(200).json(punchHistories);
+    } catch (err) {
+      res.status(500).json({ error: 'Error retrieving punch history' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
 };
-module.exports = async (req, res) => {
-    if (req.method === 'GET') {
-      try {
-        const punchHistories = await PunchHistory.find();
-        res.status(200).json(punchHistories);
-      } catch (err) {
-        res.status(500).json({ error: 'Error retrieving punch history' });
-      }
-    } else {
-      res.status(405).json({ error: 'Method Not Allowed' });
-    }
-  };
-  
