0ef9efd8a5c2ce9aac8404555c73ffc935f65f5d
Devpateldevlop <devpatel190703@gmail.com>
2025-02-11 11:53:11 +0530

    ggdd

--- api/punchHistory.js
+++ api/punchHistory.js
@@ -1,24 +1,24 @@
-// /api/punchHistory.js
-const PunchHistory = require('../model/PunchHistory');
-const mongoose = require('mongoose');
-const cors = require('cors');
-app.use(express.urlencoded({ extended: false }));
-app.use(cors({orgin:'*'}));
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  
-module.exports = async (req, res) => {
-  if (req.method === 'GET') {
-    try {
-      const punchRecords = await PunchHistory.find();
-      res.status(200).json(punchRecords);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+const connectToMongoDB = async () => {
+  const uri = process.env.MONGO_URI;
+  if (mongoose.connections[0].readyState) {
+    console.log("Already connected to MongoDB");
+    return;
   }
+
+  try {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB connected!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
+module.exports = async (req, res) => {
+  // Connect to MongoDB
+  await connectToMongoDB();
+
+  // Example response
+  res.status(200).json({ message: "Hello from Node.js API!" });
 };
