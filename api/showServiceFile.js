819cca45f312ac2d080feb75204692ce0270b497
Devpateldevlop <devpatel190703@gmail.com>
2025-02-14 11:25:59 +0530

    {commit_message}

---
+++ api/showServiceFile.js
@@ -0,0 +1,20 @@
+// api/showServiceFile.js
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  // Define the path to the service.js file
+  const filePath = path.join(__dirname, '../public/servicework/service.js');
+
+  // Read the file contents
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading the file');
+    }
+
+    // Set the response header for plain text (JavaScript)
+    res.setHeader('Content-Type', 'application/javascript');
+    res.status(200).send(data);
+  });
+};
