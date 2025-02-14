eba7db14909c5f4b8dd932aecc8f9402004ec736
Devpateldevlop <devpatel190703@gmail.com>
2025-02-14 11:32:03 +0530

    {commit_message}

--- api/showServiceFile.js
+++ api/showServiceFile.js
@@ -1,19 +1,14 @@
-// api/showServiceFile.js
-
 const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  // Define the path to the service.js file
-  const filePath = path.join(__dirname, '../public/servicework/service.js');
-
-  // Read the file contents
+  const filePath = path.join(__dirname, '..', 'public', 'servicework', 'service.js');
+  
   fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-      return res.status(500).send('Error reading the file'+err);
+      return res.status(500).send('Error reading the file');
     }
-
-    // Set the response header for plain text (JavaScript)
+    
     res.setHeader('Content-Type', 'application/javascript');
     res.status(200).send(data);
   });
