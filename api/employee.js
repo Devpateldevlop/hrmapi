35128387a574298959d7d376ea35e81c98e21e1c
Devpateldevlop <devpatel190703@gmail.com>
2025-02-20 11:34:18 +0530

    {commit_message}

--- api/employee.js
+++ api/employee.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors({
     origin: '*', // Allow all domains or restrict to your frontend's domain
-    methods: ['GET', 'POST', 'OPTIONS'], // Allowed HTTP methods
+    methods: ['GET', 'POST','PUT', 'OPTIONS'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type'], // Allowed headers
 }));
 app.options('*', cors()); // This handles preflight requests
@@ -49,6 +49,22 @@ app.post('/api/employee', async (req, res) => {
   }
 });
 
+
+app.put('/api/employee', async (req, res) => {
+
+    const { employeeCode } = req.query;  
+    const employee = await Employee.findOne({ EmployeeCode: employeeCode });
+
+    if (!employee) {
+        return res.status(404).json({ message: 'Employee not found' });
+    }
+
+    employee.profile = req.body.profile;
+    await employee.save();
+    res.status(200).json({ message: 'Employee updated successfully', data: employee });
+});
+
+
   app.get('/api/employee', async (req, res) => {
     try {
         const punchHistories = await Employee.find();
@@ -60,7 +76,7 @@ app.post('/api/employee', async (req, res) => {
   });
 app.options('*', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.status(200).end();
 });
