a004c5839667ea35ea1afc45eec49e2eb72a5da3
Devpateldevlop <devpatel190703@gmail.com>
2025-02-21 12:58:00 +0530

    {commit_message}

--- model/calendar.js
+++ model/calendar.js
@@ -8,7 +8,7 @@ mongoose.connect("mongodb+srv://pdev5771:rxHFzG2xPEkkocvM@cluster0.bso1d.mongodb
 .catch((err) => console.log('MongoDB connection error: ' + err));
 
 const calendarSchema = new mongoose.Schema({
-    date: { type: Date, required: true },
+    date: { type: String, required: true },
     type: { type: String, required: true },
     name: { type: String, required: true },
 });
