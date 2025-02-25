677849fb23f326c8e3f4b49424a8bc2d36351380
Devpateldevlop <devpatel190703@gmail.com>
2025-02-25 12:21:05 +0530

    {commit_message}

--- api/deduction.js
+++ api/deduction.js
@@ -138,3 +138,8 @@ app.options('*', (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.status(200).end();
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
