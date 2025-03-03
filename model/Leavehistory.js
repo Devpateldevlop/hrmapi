ff07266f5a686745ec8632b4c19a136a90cd0a16
Devpateldevlop <devpatel190703@gmail.com>
2025-03-03 15:22:39 +0530

    {commit_message}

--- model/Leavehistory.js
+++ model/Leavehistory.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
-
+mongoose.connect("mongodb+srv://pdev5771:rxHFzG2xPEkkocvM@cluster0.bso1d.mongodb.net", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB Connected...'))
+.catch((err) => console.log('MongoDB connection error: ' + err));
 const LeaveHistorySchema = new mongoose.Schema({
     LeaveType: { type: String, required: false },
     LeaveBalance: { type: Number, required: false },
-    FromDate: { type: Date, required: false },
+    FromDate: { type: String, required: false },
     FromDateDayType: { type: String, required: false },
-    ToDate: { type: Date, required: false },
+    ToDate: { type: String, required: false },
     ToDateDayType: { type: String, required: false },
     TotalLeaveDay: { type: Number, required: false },
     Remarks: { type: String, required: false },
-    EmailNotificationTo: { type: String, required: true },
+    EmailNotificationTo: { type: String, required: false },
     Attachment: { type: String, required: false },
     employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee' } 
 });
