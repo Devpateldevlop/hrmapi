43a91e2787580b83e8412af54a6dea3d6409598d
Devpateldevlop <devpatel190703@gmail.com>
2025-03-20 12:15:31 +0530

    df

--- api/leavehistory.js
+++ api/leavehistory.js
@@ -145,7 +145,7 @@ app.get('/api/employee/leaveHistory', async (req, res) => {
       if (!employee) return res.status(404).json({ error: 'Employee not found' });
       const LeaveH = await  Leavehistory.find({employee:employee._id})// Assuming model is in models folder
       
-      res.status(200).json({employee});
+      res.status(200).json({LeaveH});
     } catch (err) {
       res.status(500).json({ error: 'Error fetching LeaveHistory' });
     }
